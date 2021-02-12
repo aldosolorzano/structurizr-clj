6440175e540baea8507838c17ea2a4c8a80f3b83
Aldo Solorzano <aldo.solorzano@nubank.com.br>
2021-02-12 14:52:10 -0600

    Example and tests

---
+++ test/structurizr_clj/core_test.clj
@@ -0,0 +1,26 @@
+(ns structurizr-clj.core-test
+  (:require [clojure.test :refer :all]
+            [structurizr-clj.core :refer [defmodel defviews defstyles] :as structurizr]))
+
+(def my-workspace
+  (let [workspace (structurizr/new-workspace "My Workspace" " My Workspace Architecture")]
+    (defmodel [model           (structurizr/model workspace)
+               software-system (structurizr/add-software-system model "My System" "System")]
+              [yo-service (structurizr/add-container software-system "Yo" "Service" "Clojure" ["Main"])
+               database   (structurizr/add-container software-system "Database" "Main database" "Datomic" ["Database"])]
+              []
+      (structurizr/uses yo-service database "Persists data" "Datomic")
+      (defviews [views           (structurizr/views workspace)
+                 containers-view (structurizr/create-container-view views software-system "Containers" "Chargeback services")]
+        (defstyles [styles (structurizr/styles views)]
+          (doto (structurizr/add-element-style styles "Main")
+            (.background "#800080")
+            (.color "#ffffff")))
+        (doto containers-view
+          structurizr/add-all-software-systems
+          structurizr/add-all-containers)))
+    workspace))
+
+(deftest workspace-test
+  (testing "Nothing breaks and the workspace is a com.structurizr.Workspace"
+    (is (= com.structurizr.Workspace (class my-workspace)))))
