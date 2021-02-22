9f8bfe2e66b39c593491f1c464b919c44d02d3f6
Aldo Solorzano <aldo.solorzano@nubank.com.br>
2021-02-22 13:15:04 -0600

    Update test to use tags and shapes

--- test/structurizr_clj/core_test.clj
+++ test/structurizr_clj/core_test.clj
@@ -1,21 +1,33 @@
 (ns structurizr-clj.core-test
   (:require [clojure.test :refer :all]
-            [structurizr-clj.core :refer [defmodel defstyles defviews defworkspace] :as structurizr]))
+            [structurizr-clj.core :refer [defmodel defstyles defviews defworkspace] :as structurizr]
+            [structurizr-clj.shape :as structurizr.shape]
+            [structurizr-clj.tags :as structurizr.tags]))
 
 (defworkspace my-workspace
-  [workspace (structurizr/new-workspace "My Workspace" " My Workspace Architecture")]
+  [workspace (structurizr/new-workspace "Getting Started" "This is a model of my software system")]
   (defmodel [model           (structurizr/model workspace)
-             software-system (structurizr/add-software-system model "My System" "System")]
+             user            (structurizr/add-person model "User" "A user of my software system" [structurizr.tags/person])
+             software-system (structurizr/add-software-system model "Software System" "My software system")]
             [yo-service (structurizr/add-container software-system "Yo" "Service" "Clojure" ["Main"])
              database   (structurizr/add-container software-system "Database" "Main database" "Datomic" ["Database"])]
             []
+    (structurizr/uses user software-system "Uses")
     (structurizr/uses yo-service database "Persists data" "Datomic")
-    (defviews [views           (structurizr/views workspace)
-               containers-view (structurizr/create-container-view views software-system "Containers" "My services")]
+    (defviews [views                (structurizr/views workspace)
+               containers-view      (structurizr/create-container-view views software-system "Containers" "An example of Container context diagram")
+               software-system-view (structurizr/create-system-context-view views software-system "System Context" "An example of a System Context diagram")]
       (defstyles [styles (structurizr/styles views)]
+        (doto (structurizr/add-element-style styles structurizr.tags/person)
+              (structurizr/shape structurizr.shape/person))
+        (doto (structurizr/add-element-style styles "Database")
+              (structurizr/shape structurizr.shape/cylinder))
         (doto (structurizr/add-element-style styles "Main")
               (structurizr/background "#800080")
               (structurizr/color "#ffffff")))
+      (doto software-system-view
+            structurizr/add-all-software-systems
+            structurizr/add-all-people)
       (doto containers-view
             structurizr/add-all-software-systems
             structurizr/add-all-containers))))
