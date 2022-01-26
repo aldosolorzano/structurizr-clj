8d011c1fd31e88f3b0e809d6024043b74a73409c
Aldo Solorzano <aldo.solorzano@nubank.com.br>
2022-01-25 19:28:29 -0600

    Support many relationships

--- src/structurizr_clj/reader.clj
+++ src/structurizr_clj/reader.clj
@@ -11,42 +11,45 @@
 (defn uses-reader [[to-node description technology]]
   (->UsesLink to-node description technology))
 
-;; TODO
-;; 1. Allow more than one relationship
-;; 2. Allow to be able to add a relationship with containers from another sf-system
-;; 3. Support for views
-;; 4. Support for styles
-
-;; SUPORTS
-;; 1. All relationships should be in the same software system
-;; 2. Only one relationship per container
-;; 3. Relationships only happen at container level
+(defn uses! [instances coll]
+  (doall (for [{:keys [uses key]} coll]
+           (run! (fn [{:keys [to-node description technology]}]
+                   (structurizr/uses (get instances key)
+                                     (get instances to-node)
+                                     description
+                                     technology))
+                 uses))))
 
 (defn read-workspace [workspace-data]
   (let [workspace  (structurizr/new-workspace (:key workspace-data) (:description workspace-data))
         model      (structurizr/model workspace)
-        sf-systems (get-in workspace-data [:model :software-systems])]
-    (doall (for [sf-data sf-systems]
-             (let [sf                  (structurizr/add-software-system model (:key sf-data) (:description sf-data) (:tags sf-data))
-                   containers-instance (reduce (fn [initial container]
-                                                 (merge initial
-                                                        {(:key container) (structurizr/add-container sf
-                                                                                                     (:key container)
-                                                                                                     (:description container)
-                                                                                                     (:technology container)
-                                                                                                     (:tags container))}))
-                                               {}
-                                               (:containers sf-data))]
-               (run! (fn [{:keys [uses] :as container}]
-                       (when uses
-                         (structurizr/uses (get containers-instance (:key container))
-                                           (get containers-instance (:to-node uses))
-                                           (:description uses)
-                                           (:technology uses))))
-                     (:containers sf-data)))))
-    workspace))
+        sf-systems (get-in workspace-data [:model :software-systems])
+        instances  (loop [software-systems sf-systems
+                          instances        {}]
+                     (if (empty? software-systems)
+                       instances
+                       (let [sf-data             (first software-systems)
+                             sf-key              (:key sf-data)
+                             sf-instance         (structurizr/add-software-system model
+                                                                                  sf-key
+                                                                                  (:description sf-data)
+                                                                                  (:tags sf-data))
+                             containers-instance (reduce (fn [initial container]
+                                                           (merge initial
+                                                                  {(:key container) (structurizr/add-container sf-instance
+                                                                                                               (:key container)
+                                                                                                               (:description container)
+                                                                                                               (:technology container)
+                                                                                                               (:tags container))}))
+                                                         {}
+                                                         (:containers sf-data))]
 
-#_(render/workspace->json (read-sofware-systems new-example) "hola.json")
+
+                         (recur (rest software-systems)
+                                (merge instances containers-instance {sf-key sf-instance})))))]
+    (run! #(uses! instances (get % :containers)) sf-systems)
+    (uses! instances sf-systems)
+    workspace))
 
 (def new-example
   {:key         "Getting Started"
@@ -54,22 +57,20 @@
    :model      {:persons          [{:key         "User"
                                     :description "A user of my software system"
                                     :tags        #{structurizr.tags/person}
-                                    :uses        ""}]
-                :software-systems [{:key         "Software System"
+                                    :uses        [#structurizr/use ["Yo" "Uses" ""]]}]
+                :software-systems [{:key         "External software system"
+                                    :description "External system used"
+                                    :containers  []
+                                    :uses        [#structurizr/use ["Yo" "Uses" "Logging"]]}
+                                   {:key         "Software System"
                                     :description "My software system"
                                     :containers  [{:key         "Yo"
                                                    :description "Service"
                                                    :technology  "Clojure"
                                                    :tags        #{"Main"}
-                                                   :uses        #structurizr/use ["Database" "Uses" "Datomic"]}
+                                                   :uses        [#structurizr/use ["Database" "Uses" "Datomic"]]}
                                                   {:key         "Database"
                                                    :description "Main database"
                                                    :technology  "Datomic"
                                                    :tags        #{"Database"}}]}]}
-   :styles [#structurizr/style {:tag structurizr.tags/person
-                                :shape structurizr.shape/person}
-            #structurizr/style {:tag "Database"
-                                :shape structurizr.shape/cylinder}
-            #structurizr/style {:tag "Main"
-                                :background "#800080"
-                                :color "#ffffff"}]})
+   :styles []})
