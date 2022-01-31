ffa4af406e176a9ce895fe7d364d5e93a046044e
Aldo Solorzano <aldo.solorzano@nubank.com.br>
2022-01-31 15:00:04 -0600

    support persons and change relationships logic

--- src/structurizr_clj/reader.clj
+++ src/structurizr_clj/reader.clj
@@ -1,5 +1,5 @@
 (ns structurizr-clj.reader
-  (:require [structurizr-clj.core :refer [defmodel defstyles defviews defworkspace] :as structurizr]
+  (:require [structurizr-clj.core  :as structurizr]
             [structurizr-clj.shape :as structurizr.shape]
             [structurizr-clj.style :as structurizr.style]
             [structurizr-clj.tags :as structurizr.tags]
@@ -11,44 +11,70 @@
 (defn uses-reader [[to-node description technology]]
   (->UsesLink to-node description technology))
 
-(defn uses! [instances coll]
-  (doall (for [{:keys [uses key]} coll]
-           (run! (fn [{:keys [to-node description technology]}]
-                   (structurizr/uses (get instances key)
-                                     (get instances to-node)
-                                     description
-                                     technology))
-                 uses))))
-
 (defn read-workspace [workspace-data]
   (let [workspace  (structurizr/new-workspace (:key workspace-data) (:description workspace-data))
         model      (structurizr/model workspace)
         sf-systems (get-in workspace-data [:model :software-systems])
-        instances  (loop [software-systems sf-systems
-                          instances        {}]
-                     (if (empty? software-systems)
-                       instances
-                       (let [sf-data             (first software-systems)
-                             sf-key              (:key sf-data)
-                             sf-instance         (structurizr/add-software-system model
-                                                                                  sf-key
-                                                                                  (:description sf-data)
-                                                                                  (:tags sf-data))
-                             containers-instance (reduce (fn [initial container]
-                                                           (merge initial
-                                                                  {(:key container) (structurizr/add-container sf-instance
-                                                                                                               (:key container)
-                                                                                                               (:description container)
-                                                                                                               (:technology container)
-                                                                                                               (:tags container))}))
-                                                         {}
-                                                         (:containers sf-data))]
+        persons    (get-in workspace-data [:model :persons])
+        [p-instances p-relationships]
+        (loop [ppersons persons
+               instances {}
+               relationships []]
+          (if (empty? ppersons)
+            [instances relationships]
+            (let [person (first ppersons)
+                  person-instance (structurizr/add-person model
+                                                          (:key person)
+                                                          (:description person)
+                                                          (:tags person))
+                  current-relationships [(:key person) (:uses person)]]
+              (recur (rest persons)
+                     (merge instances {(:key person) person-instance})
+                     (conj relationships current-relationships)))))
+
+        [instances relationships]
+        (loop [software-systems sf-systems
+               instances        {}
+               relationships    []]
+          (if (empty? software-systems)
+            [instances relationships]
+            (let [sf-data (first software-systems)
+                  sf-key (:key sf-data)
+                  sf-instance (structurizr/add-software-system model
+                                                               sf-key
+                                                               (:description sf-data)
+                                                               (:tags sf-data))
+                  containers-instance (reduce (fn [initial container]
+                                                (merge initial
+                                                       {(:key container) (structurizr/add-container sf-instance
+                                                                                                    (:key container)
+                                                                                                    (:description container)
+                                                                                                    (:technology container)
+                                                                                                    (:tags container))}))
+                                              {}
+                                              (:containers sf-data))
+                  containers-rels (->> (:containers sf-data)
+                                       (filter :uses)
+                                       (mapv (fn [{:keys [key uses]}] [key uses])))
+                  current-relationships (if (:uses sf-data)
+                                          (concat relationships containers-rels [[sf-key (:uses sf-data)]])
+                                          (concat relationships containers-rels))]
 
 
-                         (recur (rest software-systems)
-                                (merge instances containers-instance {sf-key sf-instance})))))]
-    (run! #(uses! instances (get % :containers)) sf-systems)
-    (uses! instances sf-systems)
+              (recur (rest software-systems)
+                     (merge instances containers-instance {sf-key sf-instance})
+                     current-relationships))))
+        instances-index (merge instances p-instances)
+        full-relationships (concat relationships p-relationships)]
+
+    (doall (for [[from to-nodes] full-relationships
+                 :let [from-instance (get instances-index from)]]
+             (run! (fn [{:keys [to-node description technology]}]
+                     (structurizr/uses from-instance
+                                       (get instances-index to-node)
+                                       description
+                                       technology))
+                   to-nodes)))
     workspace))
 
 (def new-example
